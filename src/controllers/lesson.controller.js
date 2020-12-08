72a3911ee7d5009987aa3a9c7f795ea1b8d82c40
Ayobamiu <ayobamiu@gmail.com>
2020-12-08 15:17:16 +0100

    feature: Send results for a test

--- src/controllers/lesson.controller.js
+++ src/controllers/lesson.controller.js
@@ -1,4 +1,5 @@
-import Question from '../db/models/questions.model';
+import Question from "../db/models/questions.model";
+import QuizResult from "../db/models/quizResults.model";
 /**
  *Contains Lesson Controller
  *
@@ -20,15 +21,64 @@ class LessonController {
       const questions = await Question.find({ lessonId: req.params.lessonId });
 
       return res.status(200).json({
-        status: 'success',
+        status: "success",
         data: {
           questions,
         },
       });
     } catch (error) {
       return res.status(500).json({
-        status: '500 Internal server error',
-        error: 'Error Loading courses',
+        status: "500 Internal server error",
+        error: "Error Loading courses",
+      });
+    }
+  }
+
+  /**
+   * Save test result
+   * @param {Request} req - Response object.
+   * @param {Response} res - The payload.
+   * @memberof LessonController
+   * @returns {JSON} - A JSON success response.
+   *
+   */
+  static async saveTestResult(req, res) {
+    try {
+      const results = [];
+
+      req.body.results.forEach((result) => {
+        results.push({
+          questionId: result.questionId,
+          optionSelected: result.optionSelected,
+          correctOption: result.correctOption,
+          status: result.status,
+        });
+      });
+      const quizResultData = {
+        results,
+        userId: req.data.id,
+        classId: req.body.classId,
+        courseId: req.body.courseId,
+        lessonId: req.params.lessonId,
+        timeSpent: req.body.timeSpent,
+        numberOfCorrectAnswers: req.body.numberOfCorrectAnswers,
+        numberOfWrongAnswers: req.body.numberOfWrongAnswers,
+        numberOfSkippedQuestions: req.body.numberOfSkippedQuestions,
+        score: req.body.score,
+        remark: req.body.remark,
+      };
+      const quizResult = await QuizResult.create({ ...quizResultData });
+
+      return res.status(200).json({
+        status: "success",
+        data: {
+          results: quizResult,
+        },
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: "500 Internal server error",
+        error: "Error Loading courses",
       });
     }
   }
--- src/routes/lesson.route.js
+++ src/routes/lesson.route.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 import LessonController from '../controllers/lesson.controller';
+import verifyToken from '../middlewares/auth.middleware';
+import SaveQuizResults from '../validations/auth/quizResults.validator';
 // import authRouter from './auth.route';
 
 const router = Router();
 
 router.get('/:lessonId/test', LessonController.loadTest);
+router.post(
+  '/:lessonId/save-test-results',
+  SaveQuizResults.validateData(),
+  SaveQuizResults.myValidationResult,
+  verifyToken,
+  LessonController.saveTestResult,
+);
 
 export default router;
