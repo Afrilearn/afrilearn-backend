5ec34effc279c5e35ecc477d2acfbed6b9113af0
Ayobamiu <ayobamiu@gmail.com>
2021-05-06 17:44:38 +0100

    Make Primary categories free

--- src/db/models/enrolledCourses.model.js
+++ src/db/models/enrolledCourses.model.js
@@ -45,11 +45,35 @@ EnrolledCourseSchema.virtual("transaction", {
   justOne: true,
 });
 
+export const primaryCoursesIds = [
+  "5fc8cfbb81a55b4c3c19737d",
+  "5fd12c70e74b15663c5f4c6e",
+  "5fff5a67de0bdb47f826fea8",
+  "5fff5a7ede0bdb47f826fea9",
+  "5fff5aaede0bdb47f826feaa",
+  "5fff5abede0bdb47f826feab",
+];
+
 EnrolledCourseSchema.methods.toJSON = function () {
   const enrolledCourse = this;
   const enrolledCourseObject = enrolledCourse.toObject();
+  let isPrimary = false;
+  if (
+    enrolledCourseObject.courseId &&
+    primaryCoursesIds.includes(enrolledCourseObject.courseId.toString())
+  ) {
+    isPrimary = true;
+  }
+  if (
+    enrolledCourseObject.courseId &&
+    enrolledCourseObject.courseId._id &&
+    primaryCoursesIds.includes(enrolledCourseObject.courseId._id.toString())
+  ) {
+    isPrimary = true;
+  }
+
   enrolledCourseObject.paymentIsActive =
-    enrolledCourseObject.endDate > Date.now();
+    isPrimary || enrolledCourseObject.endDate > Date.now();
   return enrolledCourseObject;
 };
 
