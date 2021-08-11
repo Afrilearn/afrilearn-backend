f07ff92e28797312cf0eeacf25ed62653eb51ae0
jiokeokwuosa <okwuosachijioke@gmail.com>
2021-08-11 14:15:53 +0100

    added image url and description

--- src/db/models/pastQuestionTypes.model.js
+++ src/db/models/pastQuestionTypes.model.js
@@ -9,6 +9,14 @@ const pastQuestionTypeSchema = new mongoose.Schema(
     categoryId: {
       type: String, // exam_id on exam platform
     },
+    imageUrl: {
+      type: String,
+      default: 'https://afrilearn-media.s3.eu-west-3.amazonaws.com/past-question-images/ic_launcher_round.png'
+    },
+    description:{
+      type: String,
+      default: 'Practice to multiply your success 13,000+ questions per subject'
+    }    
   },
   {
     toJSON: { virtuals: true },
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import http from "http";
 import cors from "cors";
 import { config } from "dotenv";
 import morgan from "morgan";
@@ -7,15 +8,60 @@ import "./db";
 import v1Router from "./routes";
 import {CronJob} from 'cron';
 import ChallengeUtility from './services/challenge.services';
+import socketio  from 'socket.io';
 
 // scheduled creation of challenges on sunday
 const job = new CronJob('0 59 23 * * 0', ChallengeUtility.createNewChallenges);
 job.start();
-
  
 config();
 
 const app = express();
+
+const server = http.createServer(app);
+
+const corsOptions={
+  cors: true,
+  origins:["http://localhost:5000/api/v1/"],
+}
+const io = socketio(server);
+
+//socket
+io.on('connection', (socket) => {
+  console.log('connected to socket')
+  // socket.on('join', ({ name, room }, callback) => {
+  //   const { error, user } = addUser({ id: socket.id, name, room });
+
+  //   if(error) return callback(error);
+
+  //   socket.join(user.room);
+
+  //   socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+  //   socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+  //   io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+  //   callback();
+  // });
+
+  // socket.on('sendMessage', (message, callback) => {
+  //   const user = getUser(socket.id);
+
+  //   io.to(user.room).emit('message', { user: user.name, text: message });
+
+  //   callback();
+  // });
+
+  // socket.on('disconnect', () => {
+  //   const user = removeUser(socket.id);
+
+  //   if(user) {
+  //     io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+  //     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+  //   }
+  // })
+});
+
 const port = process.env.PORT || 5000;
 global.logger = logger;
 app.use(cors());
@@ -46,7 +92,7 @@ app.use((err, req, res) => {
 });
 
 //server
-app.listen(port, () => {
+server.listen(port, () => {
   logger.info(`Server running at port ${port} on ${process.env.NODE_ENV}`);
 });
 
