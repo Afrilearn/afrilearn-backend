309b4658c819fafb201881c0cc921e4d46a39aee
Chijioke Okwuosa <okwuosachijioke@gmail.com>
2021-09-17 00:58:54 +0100

    Update userStory.route.js

--- src/routes/userStory.route.js
+++ src/routes/userStory.route.js
@@ -3,7 +3,7 @@ import UserStoryController from "../controllers/userStory.controller";
 import upload from "../config/bucket";
 
 const router = Router();
-
-router.post("/", upload.single("image"), UserStoryController.addUserStory);
 router.get("/stories", UserStoryController.getUserStory);
+router.post("/", upload.single("image"), UserStoryController.addUserStory);
+
 export default router;
