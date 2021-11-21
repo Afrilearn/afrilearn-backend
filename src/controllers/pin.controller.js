23ac01aed30df99e61595d6205e589b5ce3a1fba
Chijioke Okwuosa <okwuosachijioke@gmail.com>
2021-11-21 23:05:22 +0100

    Update pin.controller.js

--- src/controllers/pin.controller.js
+++ src/controllers/pin.controller.js
@@ -3,52 +3,52 @@ import Pin from "../db/models/pin.model";
 import { randomString } from "../config/randomString";
 
 class PinController {
-  static async createPin(req, res) {
-    try {
-      const existingPinObjects = await Pin.find();
-      const existingPins = existingPinObjects.map((i) => {
-        return i.text;
-      });
+//   static async createPin(req, res) {
+//     try {
+//       const existingPinObjects = await Pin.find();
+//       const existingPins = existingPinObjects.map((i) => {
+//         return i.text;
+//       });
 
-      let numberOfPinsToGenerate = 1;
-      if (req.body.number && typeof req.body.number === "number") {
-        numberOfPinsToGenerate = req.body.number;
-      }
-      if (req.body.number && typeof req.body.number === "number") {
-        if (req.body.number > 500) {
-          return res.status(400).json({
-            status: "400 bad request",
-            error: "You can not generate more than 500 pins at once.",
-          });
-        }
-      }
-      const arr = [];
-      for (let index = 0; index < numberOfPinsToGenerate; index++) {
-        let text = randomString(9);
-        if (existingPins.includes(text)) {
-          text = randomString(9);
-        } else {
-          arr.push({ text });
-        }
-      }
-      await Pin.insertMany(arr, { rawResult: true }, function (error, docs) {
-        const pins = docs.ops.map((i) => {
-          return i.text;
-        });
-        return res.status(200).json({
-          status: "success",
-          data: {
-            pins,
-          },
-        });
-      });
-    } catch (error) {
-      return res.status(500).json({
-        status: "500 Internal server error",
-        error: "Error creating pin",
-      });
-    }
-  }
+//       let numberOfPinsToGenerate = 1;
+//       if (req.body.number && typeof req.body.number === "number") {
+//         numberOfPinsToGenerate = req.body.number;
+//       }
+//       if (req.body.number && typeof req.body.number === "number") {
+//         if (req.body.number > 500) {
+//           return res.status(400).json({
+//             status: "400 bad request",
+//             error: "You can not generate more than 500 pins at once.",
+//           });
+//         }
+//       }
+//       const arr = [];
+//       for (let index = 0; index < numberOfPinsToGenerate; index++) {
+//         let text = randomString(9);
+//         if (existingPins.includes(text)) {
+//           text = randomString(9);
+//         } else {
+//           arr.push({ text });
+//         }
+//       }
+//       await Pin.insertMany(arr, { rawResult: true }, function (error, docs) {
+//         const pins = docs.ops.map((i) => {
+//           return i.text;
+//         });
+//         return res.status(200).json({
+//           status: "success",
+//           data: {
+//             pins,
+//           },
+//         });
+//       });
+//     } catch (error) {
+//       return res.status(500).json({
+//         status: "500 Internal server error",
+//         error: "Error creating pin",
+//       });
+//     }
+//   }
   static async authorizeWithPin(req, res) {
     try {
       const pin = await Pin.findOne({ text: req.params.text });
