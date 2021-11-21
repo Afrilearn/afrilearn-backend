f96305c41426ed857dbabe28e5e14f0838af31c6
Chijioke Okwuosa <okwuosachijioke@gmail.com>
2021-11-21 23:05:52 +0100

    Update pin.route.js

--- src/routes/pin.route.js
+++ src/routes/pin.route.js
@@ -4,6 +4,6 @@ import PinController from "../controllers/pin.controller";
 const router = Router();
 
 router.post("/autorize/:text", PinController.authorizeWithPin);
-router.post("/generate", PinController.createPin);
+// router.post("/generate", PinController.createPin);
 export default router;
 //shsjhsj
