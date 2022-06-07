ad9fd70bd947c57071203a1652338c2e74b2db27
micheaol <micheaol80@gmail.com>
2022-06-07 22:06:38 +0100

    Set up for swaga documentation

--- src/gcpSetup.js
+++ src/gcpSetup.js
@@ -1,2 +1,2 @@
-var fs=require('fs');
-fs.writeFile(process.env.GCP_KEY_FILE, process.env.GCP_CRED, (err) => {});
+// var fs=require('fs');
+// fs.writeFile(process.env.GCP_KEY_FILE, process.env.GCP_CRED, (err) => {});
