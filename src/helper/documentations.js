95c6a15b925a2db443bf48e690f43e3733ecd6bc
micheaol <micheaol80@gmail.com>
2022-06-09 07:27:11 +0100

    Add url for production

--- src/helper/documentations.js
+++ src/helper/documentations.js
@@ -1,3 +1,4 @@
+
 const swaggerDocumentation = {
     openapi: "3.0.0",
     info : {
@@ -8,18 +9,44 @@ const swaggerDocumentation = {
 
     servers: [
         {
-            url: "http://localhost:5000",
+            url: "http://localhost:5000/api/v1",
             description: "Development Enviroment"
         },
         {
-            url: "http://localhost:5000/api/v1",
+		url: "https://afrilearn-backend-01.herokuapp.com/api/v1",
             description: "Production Enviroment"
         },
     ],
 
+    tags: [
+        {
+            name: "auth",
+            description: "Lessons routes"
+        },
+    ],
+
     paths: {
-        "/lessons": {
-            get: {}
+        "/auth/roles": {
+            get: {
+                tags: ["auth"],
+                description: "List of all the lessons' videos",
+                responses: {
+                    200: {
+                        description: "Ok",
+                        content: {
+                            "application/json": {
+                                schema: {
+                                    type: "object",
+                                    example: {
+                                        count: 0,
+                                        lesson: [],
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 };
